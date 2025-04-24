65a7548abc6e871726e5cd22039e2d52198da6c2
PBoo777 <konolktnv@mail.ru>
2025-04-24 17:46:49 +0300

    ФЗ 6 коммит 2 (исправление для автотестов1)

--- src/Test/ManagersTest.java
+++ src/Test/ManagersTest.java
@@ -1,7 +1,6 @@
 package Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import TaskService.Managers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,13 +9,11 @@ class ManagersTest {
 
     @Test
     public void ManagersReturnNotNullObjects() {
-
         Assertions.assertNotNull(Managers.getDefault(), "Объект не найден.");
-        assertEquals("TaskService.InMemoryTaskManager", Managers.getDefault().getClass().getName()
-                , "Объект другого типа.");
+        assertEquals("TaskService.InMemoryTaskManager", Managers.getDefault().getClass().getName(),
+                 "Объект другого типа.");
         assertNotNull(Managers.getDefaultHistory(), "Объект не найден.");
-        assertEquals("TaskService.InMemoryHistoryManager", Managers.getDefaultHistory().getClass().getName()
-                , "Объект другого типа.");
-
+        assertEquals("TaskService.InMemoryHistoryManager", Managers.getDefaultHistory().getClass().getName(),
+                 "Объект другого типа.");
     }
 }
